5cf0b21a13cc53018607bb655b184cfc93068b5b
Joost van Vark <mail@jvanvark.nl>
2020-12-29 15:06:17 +0100

    add routes

---
+++ src/Route.elm
@@ -0,0 +1,34 @@
+module Route exposing (Route(..), fromUrl, routeToString)
+
+import Browser.Navigation as Nav
+import Url exposing (Url)
+import Url.Builder
+import Url.Parser as Parser exposing (Parser, oneOf, s)
+
+
+type Route
+    = Home
+
+
+parser : Parser (Route -> a) a
+parser =
+    oneOf
+        [ Parser.map Home Parser.top
+        ]
+
+
+fromUrl : Url -> Maybe Route
+fromUrl =
+    Parser.parse parser
+
+
+routeToString : Route -> String
+routeToString route =
+    Url.Builder.relative (routeToPieces route) []
+
+
+routeToPieces : Route -> List String
+routeToPieces route =
+    case route of
+        Home ->
+            []
