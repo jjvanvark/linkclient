ca19047f334a0a6da8c76ddbc5d17fd79a6d0063
Joost van Vark <mail@jvanvark.nl>
2020-12-29 15:06:32 +0100

    hookup pages, login and routes

--- src/Main.elm
+++ src/Main.elm
@@ -3,26 +3,56 @@ module Main exposing (main)
 import Browser exposing (Document)
 import Browser.Navigation as Nav
 import Effect exposing (Effect)
+import Graphql.Http exposing (Error)
 import Html exposing (text)
+import Login
+import Page exposing (Page)
+import Route exposing (Route(..))
 import Session exposing (Session)
+import Start exposing (Start)
+import StringValidation as V
 import Url exposing (Url)
 
 
 type alias Model =
     { key : Nav.Key
     , session : Session
+    , login : Login.Model
+    , page : Page
     }
 
 
 init : Url -> Nav.Key -> ( Model, Effect Msg )
 init url key =
-    ( Model key Session.init, Effect.none )
+    let
+        ( loginModel, loginEffect ) =
+            Login.init
+
+        ( model, eff ) =
+            changeRouteTo
+                (Route.fromUrl url)
+                { key = key
+                , session = Session.init
+                , login = loginModel
+                , page = Page.blank
+                }
+    in
+    ( model
+    , Effect.batch
+        [ Effect.map GotLoginMsg loginEffect
+        , eff
+        , Effect.starter ReceivedStart
+        ]
+    )
 
 
 type Msg
     = Ignored String
     | ChangedUrl Url
     | ClickedLink Browser.UrlRequest
+    | GotPageMsg Page.Msg
+    | GotLoginMsg Login.Msg
+    | ReceivedStart (Result (Error Start) Start)
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
@@ -42,6 +72,36 @@ update msg model =
         ChangedUrl url ->
             ( model, Effect.none )
 
+        GotPageMsg pageMsg ->
+            Page.update pageMsg model.page
+                |> fromPage model
+
+        GotLoginMsg loginMsg ->
+            let
+                ( updatedModel, effect ) =
+                    Login.update loginMsg model.login
+            in
+            ( { model | login = updatedModel }, Effect.map GotLoginMsg effect )
+
+        ReceivedStart (Err err) ->
+            ( model, Effect.logout )
+
+        ReceivedStart (Ok { me }) ->
+            ( model, Effect.login me )
+
+
+changeRouteTo : Maybe Route -> Model -> ( Model, Effect Msg )
+changeRouteTo maybeRoute model =
+    Page.changeRouteTo maybeRoute model.page
+        |> fromPage model
+
+
+fromPage : Model -> ( Page, Effect msg ) -> ( Model, Effect Msg )
+fromPage model ( page, effect ) =
+    ( { model | page = page }
+    , Effect.none
+    )
+
 
 subscriptions : Model -> Sub msg
 subscriptions _ =
@@ -50,7 +110,12 @@ subscriptions _ =
 
 view : Model -> Document Msg
 view model =
-    { title = "Link O Rama", body = [ text "Welcome" ] }
+    let
+        { title, body } =
+            Page.view model.page
+                |> Page.mapDocument GotPageMsg
+    in
+    { title = "Link O Rama :: " ++ title, body = body ++ [ Html.map GotLoginMsg (Login.view model.login) ] }
 
 
 main : Program () Model Msg
